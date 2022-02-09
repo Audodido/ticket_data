b82c1c3e64a154c4c8b2caaa1c419a10aebe454a
connor <chanwick@gmail.com>
2022-02-09 10:54:39 -0500

    added counter

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import time
 import re
 
 file_location = "/Users/connorhanwick/Desktop/SFAB - IT Support/Tickets/Ticketing Interface.csv"
-keywords = ["printer", "mac", "lenovo", "windows","chrome","lexmark","outlook"]
-
+keywords = ["printer", "mac", "apple", "lenovo", "windows","chrome","lexmark","outlook"]
+tickets_count = 0
 
 def parse_files(file, keyword):
+    index = 1
+
     with open(file, 'r') as f:
         reader = csv.reader(f)
 
@@ -16,16 +18,17 @@ def parse_files(file, keyword):
             line = r[1]
             check = re.search(keyword, line, re.IGNORECASE)
             if check != None:
+                global tickets_count
+                tickets_count += 1
                 print(line)
 
             
-    
 def stat_puller(list):
     """Takes a list of keywords and returns if/how frequently each occurs in a csv"""
 
-    index = 1
+
     for keyword in list:
-        print(f' - - - -{keyword.upper()} - - - - ')
+        print(f' - - - -{keyword.upper()} {tickets_count} - - - - ')
         parse_files(file_location, keyword)
 
 
